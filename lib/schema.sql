2f9486124fc784bc6ce3188125b2265f0e9f7ab4
Trevis McGill <trevismcgill25@gmail.com>
2020-08-26 16:23:00 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -17,15 +17,21 @@ name TEXT
 
 CREATE TABLE books (
 id INTEGER PRIMARY KEY,
-
+title TEXT,
+year INT,
+series_id INT,
 );
 
 CREATE TABLE characters (
 id INTEGER PRIMARY KEY,
-
+name TEXT,
+species TEXT,
+motto TEXT,
+author_id INT
 );
 
 CREATE TABLE character_books (
 id INTEGER PRIMARY KEY,
-
+book_id INT,
+character_id INT
 );
