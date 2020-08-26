96fe7faeac3566cd72b894a45dd0bf92a3f457cb
Trevis McGill <trevismcgill25@gmail.com>
2020-08-26 16:20:53 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE series (
+id INTEGER PRIMARY KEY
+
+);
+
+CREATE TABLE subgenres (
+
+
+);
+
+CREATE TABLE authors (
+
+
+);
+
+CREATE TABLE books (
+
+
+);
+
+CREATE TABLE characters (
+
+
+);
+
+CREATE TABLE character_books (
+
+
+);
