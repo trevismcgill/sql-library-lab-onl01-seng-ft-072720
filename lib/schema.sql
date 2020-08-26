7aa7a55cd2ed391ddfc7ce8c962791f1937c4025
Trevis McGill <trevismcgill25@gmail.com>
2020-08-26 16:21:32 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,29 +1,31 @@
 CREATE TABLE series (
-id INTEGER PRIMARY KEY
-
+id INTEGER PRIMARY KEY,
+title TEXT,
+author_id INTEGER
+subgenre_id INT
 );
 
 CREATE TABLE subgenres (
-
+id INTEGER PRIMARY KEY,
 
 );
 
 CREATE TABLE authors (
-
+id INTEGER PRIMARY KEY,
 
 );
 
 CREATE TABLE books (
-
+id INTEGER PRIMARY KEY,
 
 );
 
 CREATE TABLE characters (
-
+id INTEGER PRIMARY KEY,
 
 );
 
 CREATE TABLE character_books (
-
+id INTEGER PRIMARY KEY,
 
 );
