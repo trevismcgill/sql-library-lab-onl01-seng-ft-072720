914e7ae62959c9e0f7d4be5a6fe32edcda252b01
Trevis McGill <trevismcgill25@gmail.com>
2020-08-26 17:15:30 +0000

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE series (
+id INTEGER PRIMARY KEY,
+title TEXT,
+author_id INT,
+subgenre_id INT
+);
+
+CREATE TABLE subgenres (
+id INTEGER PRIMARY KEY,
+name TEXT
+);
+
+CREATE TABLE authors (
+id INTEGER PRIMARY KEY,
+name TEXT
+);
+
+CREATE TABLE books (
+id INTEGER PRIMARY KEY,
+title TEXT,
+year INT,
+series_id INT
+);
+
+CREATE TABLE characters (
+id INTEGER PRIMARY KEY,
+name TEXT,
+species TEXT,
+motto TEXT,
+author_id INT
+);
+
+CREATE TABLE character_books (
+id INTEGER PRIMARY KEY,
+book_id INT,
+character_id INT
+);
